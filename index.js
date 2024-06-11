af88c1dd4037a36a5ce2a4f1068d50e713faeb49
Marksman4 <127570660+Marksman4@users.noreply.github.com>
2024-06-11 16:23:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+
+function showResult(){
+    var yourname =prompt("What is your name");
+    yourname =yourname.toUpperCase();
+    var lovername =prompt("What is your lover name")
+    lovername =lovername.toUpperCase();
+    var randomnumber =Math.floor(Math.random() * 100) + 1;
+
+    if( randomnumber > 0 && randomnumber < 10 ){
+        var score =document.getElementById("score").innerHTML="HI! " + yourname + ", YOU LOVE " + lovername + " " + randomnumber + "% PERCENT. Thats too poor 💩, Say i love you to your lover today!" ;
+
+    }
+
+
+
+    if( randomnumber > 10 && randomnumber < 20 ){
+        var score =document.getElementById("score").innerHTML="HI! " + yourname + ", YOU LOVE " + lovername + " " + randomnumber + "% PERCENT. hmmm warisdis 😈 Send your lover a love message today! " ;
+
+    }
+
+    else if( randomnumber > 20 && randomnumber < 30 ){
+        var score =document.getElementById("score").innerHTML="HI! " + yourname + ", YOU LOVE " + lovername + " " + randomnumber + "% PERCENT. Ewooooh 🤬 Oya dash your lover a Surprise Gift Today!" ;
+
+    }
+    else if( randomnumber > 30 && randomnumber < 40 ){
+        var score =document.getElementById("score").innerHTML="HI! " + yourname + ", YOU LOVE " + lovername + " " + randomnumber + "% PERCENT. See as i dey look U 🐵, Nawaa for You oooh" ;
+
+    }
+    
+
+    else if( randomnumber > 40 && randomnumber < 50 ){
+        var score =document.getElementById("score").innerHTML="HI! " + yourname + ", YOU LOVE " + lovername + " " + randomnumber + "% PERCENT. 😉 For your Mind you dey try abi, Better Person"  ;
+
+    }
+
+    else if( randomnumber > 50 && randomnumber < 60 ){
+        var score =document.getElementById("score").innerHTML="HI! " + yourname + ", YOU LOVE " + lovername + " " + randomnumber + "% PERCENT. 🥰 Na love Go Kill You" ;
+
+    }
+
+    else if( randomnumber > 60 && randomnumber < 70 ){
+        var score =document.getElementById("score").innerHTML="HI! " + yourname + ", YOU LOVE " + lovername + " " + randomnumber + "% PERCENT. 😜 Pablo and Pablet 😋  " ;
+
+    }
+    else if( randomnumber > 70 && randomnumber < 80 ){
+        var score =document.getElementById("score").innerHTML="HI! " + yourname + ", YOU LOVE " + lovername + " " + randomnumber + "% PERCENT. 😘 Chairman lovers council, Come carry cup 💋" ;
+
+    }
+    else if( randomnumber > 80 && randomnumber < 90 ){
+        var score =document.getElementById("score").innerHTML="HI! " + yourname + ", YOU LOVE " + lovername + " " + randomnumber + "% PERCENT. 💌 Honourable Minister of love Affairs 💕 " ;
+
+    }
+    else {
+        var score =document.getElementById("score").innerHTML="HI! " + yourname + ", YOU LOVE " + lovername + " " + randomnumber + "% PERCENT. 💝 National President of Love Affair, 😇 " ;
+
+    }
+    return document.querySelector("button").innerHTML="PLAY AGAIN!";
+
+    
+
+    
+
+}
+
+
